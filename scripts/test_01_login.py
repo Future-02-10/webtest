a08da9c2c8b85c61fb84d565e6bf0b893a252faf
Sunny-Moom <khgkhh@foxmail.com>
2025-01-02 10:47:06 +0800

    初始

--- scripts/test_01_login.py
+++ scripts/test_01_login.py
@@ -12,6 +12,7 @@ class TestLogin:
     def setup_class(self):
         # ②创建浏览器驱动对象
         self.driver = DriveUtils.get_driver()
+        self.driver.implicitly_wait(2)
 
     # 类级别后置方法：关闭浏览器
     def teardown_class(self):
@@ -22,16 +23,27 @@ class TestLogin:
     def setup_method(self):
         # ③访问页面
         self.driver.get("http://test.sunnymoom.top/home/index/login")
-        time.sleep(3)
+        time.sleep(1)
 
     # 方法级别后置方法：每个用例执行完成之后，停留2S
     def teardown_method(self):
         # ⑤暂停几秒钟
         time.sleep(1)
 
+
+    @allure.title("验证登录成功")
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test01_login_suc(self):
+        with allure.step("测试步骤一：输入正确的用户名、密码、验证码，点击登录"):
+            LoginPage().login("111","111")
+        with allure.step("测试步骤二：断言"):
+            is_suc=is_text_present(self.driver,"退出")
+            assert is_suc
+        allure.attach(self.driver.get_screenshot_as_png(),name="登录成功截图",attachment_type=allure.attachment_type.PNG)
+
     # 测试用例：登录功能
     @pytest.mark.parametrize("case_name,user,pwd,exp_el,expect",read_json("login_data"))
-    def test01_login_abnormal(self,case_name,user,pwd,exp_el,expect):
+    def test02_login_abnormal(self,case_name,user,pwd,exp_el,expect):
         allure.dynamic.title(case_name)
         allure.dynamic.severity("normal")
         with allure.step("测试步骤一：输入用户名、密码、验证码，点击登录"):
@@ -39,13 +51,3 @@ class TestLogin:
         with allure.step("测试步骤二：断言"):
             msg=get_el_text(self.driver,exp_el,expect)
             assert expect in msg
-
-    @allure.title("验证登录成功")
-    @allure.severity(allure.severity_level.BLOCKER)
-    def test02_login_suc(self):
-        with allure.step("测试步骤一：输入正确的用户名、密码、验证码，点击登录"):
-            LoginPage().login("111","111")
-        with allure.step("测试步骤二：断言"):
-            is_suc=is_text_present(self.driver,"退出")
-            assert is_suc
-        allure.attach(self.driver.get_screenshot_as_png(),name="登录成功截图",attachment_type=allure.attachment_type.PNG)
