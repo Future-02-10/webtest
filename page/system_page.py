2979d404829c0170b862f15e2f860ff15a81468c
Sunny-Moom <khgkhh@foxmail.com>
2025-01-03 17:11:57 +0800

    后端登录

--- page/system_page.py
+++ page/system_page.py
@@ -1,3 +1,21 @@
 from selenium.webdriver.common.by import By
 from base.base_page import BasePage
 
+class SystemPage(BasePage):
+    def __init__(self):
+        super().__init__()
+        self.user = (By.ID, "username")
+        self.pwd = (By.ID, "password")
+        self.cpacha = (By.ID, "cpacha")
+        self.login_btn = (By.ID,"submit-btn")
+
+    def login(self,user,pwd,cpacha):
+        self.input_text(self.find_el(self.user),user)
+        self.input_text(self.find_el(self.pwd),pwd)
+        self.input_text(self.find_el(self.cpacha), cpacha)
+        self.find_el(self.login_btn).click()
+
+
+if __name__ == '__main__':
+    print(SystemPage().user)
+    print(*SystemPage().user)
